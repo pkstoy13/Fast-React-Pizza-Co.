0811e8f786760c852424428357e57661578451dd
Philip Stoyanov <pkstoyanov1@gmail.com>
2023-12-21 21:19:34 -0500

    introduction of components

--- src/index.js
+++ src/index.js
@@ -49,13 +49,47 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
 }
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  /*if (hour >= openHour && hour <= closeHour) {
+    alert("We're currently open!");
+  } else {
+    alert("Sorry we're closed");
+  }*/
+  //console.log(hour);
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+  );
+  //return React.createElement("footer", null, "We're currently open!");
+}
+
 //component
 function Pizza() {
   return (
